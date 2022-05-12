70df7dfd8158a0636f30bc9585904a7700d4b9f9
Ubuntu <ubuntu@ip-172-31-20-38.ec2.internal>
2022-05-12 19:14:43 +0000

    Changes for interview.

--- api/items/models.py
+++ api/items/models.py
@@ -8,13 +8,7 @@ class Item(BaseModel):
     base_price = models.DecimalField(max_digits=7, decimal_places=2)
     tax = models.DecimalField(max_digits=7, decimal_places=2)
     final_price = models.DecimalField(max_digits=7, decimal_places=2)
+    # Create a field to store the company name for each item. Think about the most ideal way. 
     
-    def save(self, *args, **kwargs):
-        if self.base_price:
-            self.final_price = self.base_price * (1 + self.tax)
-        else:
-            self.final_price = self.base_price = 0
-        super(Item, self).save(*args, **kwargs)
-        
     def __str__(self):
         return self.name
--- api/items/signals.py
+++ api/items/signals.py
@@ -6,13 +6,10 @@ from .models import Item
 @receiver(post_save, sender=Item)
 def create_instance(sender, instance, created, **kwargs):
     if created:
-        actual_final_price = instance.base_price * (2 + instance.tax)
+        actual_final_price = instance.base_price * (1 + instance.tax)
         instance.final_price = actual_final_price
-        instance.save()
-  
+
 @receiver(post_save, sender=Item)
 def save_profile(sender, instance, **kwargs):
-        actual_final_price = instance.base_price * (2 + instance.tax)
-        print(actual_final_price)
+        actual_final_price = instance.base_price * (1 + instance.tax)
         instance.final_price = actual_final_price
-        instance.save()
